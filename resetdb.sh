85aecdd5a734454d44770d8e32d8c8e4dec504ea
David Jones <djones6@uk.ibm.com>
2017-02-17 11:07:44 +0000

    Fix execution of scripts when not in same dir

--- resetdb.sh
+++ resetdb.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./mongo_ramdisk.sh stop
-./mongo_ramdisk.sh start && ./loaddb-nodejs.sh
+# Determine location of this script
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+${SCRIPT_DIR}/mongo_ramdisk.sh stop
+${SCRIPT_DIR}/mongo_ramdisk.sh start && ${SCRIPT_DIR}/loaddb-nodejs.sh
 
