0eee456a2da8a6659e78d6436d32e4bc6dd83890
David Jones <djones6@uk.ibm.com>
2017-02-17 12:35:41 +0000

    Fixes for Mac

--- runnode_cache.sh
+++ runnode_cache.sh
@@ -5,17 +5,17 @@ DB_HOST=localhost
 cd ~/acmeair/acmeair-nodejs
 
 # Enable caching
-sed -i -e's#\(.*"useFlightDataRelatedCaching" :\).*#\1 true, #' settings.json
+sed -i .bak -e's#\(.*"useFlightDataRelatedCaching" :\).*#\1 true, #' settings.json
 
 # Configure database host
-sed -i -e"s#\(.*\"mongoHost\" :\).*#\1 \"${DB_HOST}\", #" settings.json
+sed -i .bak -e"s#\(.*\"mongoHost\" :\).*#\1 \"${DB_HOST}\", #" settings.json
 
 # Disable logging
-sed -i -e's#\(.*"loggerLevel" :\).*#\1 "WARN", #' settings.json
-sed -i -e's#\(.*"useDevLogger" :\).*#\1 false #' settings.json
+sed -i .bak -e's#\(.*"loggerLevel" :\).*#\1 "WARN", #' settings.json
+sed -i .bak -e's#\(.*"useDevLogger" :\).*#\1 false #' settings.json
 
 echo "Resetting DB to clean state"
-ssh $DB_HOST -t ~/acmeair/acmeair-scripts/resetdb.sh
+ssh $DB_HOST -t "~/acmeair/acmeair-scripts/resetdb.sh"
 
 echo "Starting Node (caching) implementation"
 exec node app.js
--- runswift.sh
+++ runswift.sh
@@ -5,7 +5,7 @@ export DB_PORT=27017
 export PORT=9080
 
 echo "Resetting DB to clean state"
-ssh $DB_HOST -t ~/acmeair/acmeair-scripts/resetdb.sh
+ssh $DB_HOST -t "~/acmeair/acmeair-scripts/resetdb.sh"
 
 echo "Starting Swift implementation"
 cd ~/acmeair/acmeair-swift
